2cae1db8aec1144ea5c3c2872693958f8b10ceb1
praveen <praveenpkbadiger@gmail.com>
2022-10-14 20:54:17 +0530

    ADDED:[Praveen]Creating AddressBook JPA Entity

---
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressBook.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.addressbookapp.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "adressbook")
+public class AddressBook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long pinCode;
+
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+
+
+}
