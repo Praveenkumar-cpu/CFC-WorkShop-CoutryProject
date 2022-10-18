d694ab11925f936ddcbaddf150af87959cc461e2
praveen <praveenpkbadiger@gmail.com>
2022-10-18 17:44:19 +0530

    ADDED:[Praveen] Excpetion Handled

--- src/main/java/com/bridgelabz/addressbookapp/exception/ResourceNotFoundException.java
+++ src/main/java/com/bridgelabz/addressbookapp/exception/ResourceNotFoundException.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.addressbookapp.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
@@ -30,6 +31,7 @@ public class ResourceNotFoundException extends RuntimeException {
     public Object getFieldValue() {
         return fieldValue;
     }
+
 
 
 
